b47c233ee251747dc7212c3596f9bc98810a0948
Mr-FuzzyPenguin <scokro7141@gmail.com>
2022-10-03 18:13:47 -0400

    Added fonts and a few changes to the UI.

--- latex-editor.js
+++ latex-editor.js
@@ -8,11 +8,12 @@ latex_input.value = "";
 
 katex.throwOnError = false;
 
+// Katex rendering the output.
 function convert() {
     const userInput = latex_input.value;
     try
     {
-        katex.render(userInput, result, {"displayMode":true,"leqno":false,"fleqn":true,"throwOnError":true,"errorColor":"#cc0000","strict":"warn","output":"htmlAndMathml","trust":false,"macros":{"\\f":"#1f(#2)"}})
+        katex.render(userInput, result, {"displayMode":true,"leqno":false,"fleqn":true,"throwOnError":true,"errorColor":"#cc0000","strict":"warn","output":"htmlAndMathml","trust":false,"macros":{}})
     }
     catch (e) {
         if (e instanceof katex.ParseError) {
@@ -21,6 +22,7 @@ function convert() {
     }
 }
 
+
 latex_input.addEventListener("input", convert);
 
 if (latex_input.value != '')
@@ -28,23 +30,41 @@ if (latex_input.value != '')
     convert();
 }
 
-// MQ
-let mq = document.getElementById('mq');
-let mq_input = MQ.MathField(mq, {
+// MQ on Enter keypress
+let mq = $('#mq');
+let mq_input = MQ.MathField(mq[0], {
     handlers: {
       edit: function() {
-        latex = mq_input.latex()
-        const userInput = latex_input.value
+        // search for all variables, and change font to KaTeX
+        console.log("please work.")
+        // for (let i of document.querySelectorAll(".mq-math-mode var")){i.style.font = "italic 1.21em KaTeX_Main, Times New Roman, serif"};
       },
       enter: function()
       {
-        // hacky way of making the rendered latex, katexbox and mathquill update all at the same time, and delete
+        // get Latex value
+        latex = mq_input.latex()
+        // go to right end.
+        mq_input.moveToRightEnd()
+        // add a space, and add a newline
+        mq_input.keystroke('Space');
+        // send it to variable latex_input so convert() can detect the global variable
         latex_input.value += latex + "\\newline \n"
+        // convert the Katex.
         convert();
+        // clear all
         mq_input.select();
         mq_input.keystroke('Backspace');
+
       }
     }
 });
 
+mq.on('focusout.animation', function()
+{
+  // alert("lost focus. current Latex:"+latex)
 
+  if (!mq_input.latex())
+  {
+    mq_input.write("\\text{Type something in here...}")
+  }
+});
